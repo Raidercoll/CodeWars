7e3c240497cbaf8c96b07822d7d1910dad0fbf88
Raidercoll <davi.nunes@edu.pucrs.br>
2023-09-29 14:22:45 -0300

    push

--- Java/07/NumberIntring/src/Main.java
+++ Java/07/NumberIntring/src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
+    public int NumberinString(String s) {
+        int bigger = 0;
+
+        for(int i = 0; i < s.length(); i ++){
+            
+        }
+
+        return 0;
+    } 
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
